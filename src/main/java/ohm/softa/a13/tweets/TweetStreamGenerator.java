f620c36d7e345d319074f90821d01c2abfb66e57
MemphisCornelius <accounts.marvi@mailbox.org>
2023-07-03 23:33:47 +0200

    Generators

--- src/main/java/ohm/softa/a13/tweets/TweetStreamGenerator.java
+++ src/main/java/ohm/softa/a13/tweets/TweetStreamGenerator.java
@@ -1,8 +1,14 @@
 package ohm.softa.a13.tweets;
 
+import com.google.gson.Gson;
 import ohm.softa.a13.model.Tweet;
 import org.apache.commons.lang3.NotImplementedException;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -26,6 +32,13 @@ public class TweetStreamGenerator {
 		// TODO use gson to read in the tweets from the referenced path
 		// use the getResourceAsStream method to access files in the `resources` directory
 
-		throw new NotImplementedException("Not implemented");
+		Gson gson = new Gson();
+		Reader reader = new InputStreamReader(
+			TweetStreamGenerator.class.getResourceAsStream(path)
+		);
+
+		Tweet[] tweets = gson.fromJson(reader, Tweet[].class);
+
+		return new TweetStreamGenerator(List.of(tweets));
 	}
 }
