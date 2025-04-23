2f9e919d267b749992a97a32343bebb103d345e8
Marvi Krich <accounts.marvi@mailbox.org>
2025-04-23 12:47:23 +0200

    add solution (i guess)

--- src/main/java/ohm/softa/a13/tweets/TrumpTweetStats.java
+++ src/main/java/ohm/softa/a13/tweets/TrumpTweetStats.java
@@ -10,38 +10,29 @@ import java.util.stream.Stream;
 public class TrumpTweetStats {
 
     public static Map<String, Long> calculateSourceAppStats(Stream<Tweet> tweetStream) {
-        /* TODO group the tweets by the `sourceApp` they were created with and count how many it were per `sourceApp` */
 		return tweetStream
 			.collect(Collectors.groupingBy(Tweet::getSourceApp, Collectors.counting()));
     }
 
     public static Map<String, Set<Tweet>> calculateTweetsBySourceApp(Stream<Tweet> tweetStream) {
-        /* TODO group the tweets by the `sourceApp`
-         * collect the tweets in `Set`s for each source app */
         return tweetStream
 			.collect(Collectors.groupingBy(Tweet::getSourceApp, Collectors.toSet()));
     }
 
     public static Map<String, Integer> calculateWordCount(Stream<Tweet> tweetStream, List<String> stopWords) {
-        /* Remark: implement this method at last */
-        /* TODO split the tweets, lower them, trim them, remove all words that are in the `stopWords`,
-         * reduce the result to a Map<String, Integer> to count how often each word were in the tweets
-         * optionally you could filter for all words that were used more than 10 times */
 		return tweetStream
+			.parallel()
 			.map(Tweet::getText)
 			.map(tweet -> tweet.split("( )+"))
 			.flatMap(Arrays::stream)
+			.map(String::toLowerCase)
 			.filter(word -> !stopWords.contains(word))
 			.reduce(new HashMap<>(), (HashMap<String, Integer> map, String word) -> {
-				map.putIfAbsent(word, 0);
-				map.put(word, map.get(word)+1);
+					map.put(word, 1);
 				return map;
-			}, (m1, m2) -> {
-				m1.forEach((k, v) -> {
-					m2.putIfAbsent(k, 0);
-					m2.put(k, m2.get(k) + v);
-				});
-				return m2;
-			});
+			}, (m1, m2) -> m1
+			);//.entrySet()
+			//.stream().filter(e -> e.getValue() > 10)
+			//.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 }
